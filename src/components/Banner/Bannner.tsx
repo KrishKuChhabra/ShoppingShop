a7e0edcb08bb29e34199c544ff0a5253bb29cbe2
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2023-09-29 13:52:28 +0530

    some minor changes

--- src/components/Banner/Bannner.tsx
+++ src/components/Banner/Bannner.tsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import bannerone from "../../../public/images/bannerone.jpg";
 import bannertwo from "../../../public/images/bannertwo.jpg";
 import bannerthree from "../../../public/images/bannerthree.jpg";
-import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import Image from "next/image";
 import BannerText from "./BannerText";
 
@@ -15,7 +15,7 @@ const Banner = () => {
         className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute left-2 top-1/2"
         onClick={onClick}
       >
-        <PiCaretLeftLight />
+        <IoIosArrowBack />
       </div>
     );
   };
@@ -26,7 +26,7 @@ const Banner = () => {
         className="p-3 bg-slate-100 hover:text-orange-600 hover:bg-white cursor-pointer duration-200 rounded-full text-2xl flex items-center justify-center z-20 absolute right-2 top-1/2"
         onClick={onClick}
       >
-        <PiCaretRightLight />
+        <IoIosArrowForward />
       </div>
     );
   };
